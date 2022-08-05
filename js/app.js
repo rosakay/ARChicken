dce8cdc969058af2e075e0891e64431450295ab7
rosakay <rosakay11@gmail.com>
2022-08-05 12:47:23 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Builds.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/20220805.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
